344583f32ca7ecbf3722818c1a066c8c563a609c
Willian <willian-ottoni@hotmail.com>
2012-03-25 10:44:26 -0300

    Adicionado as variaveis, EditText, Button e float

--- src/br/com/imcProject/IMCActivity.java
+++ src/br/com/imcProject/IMCActivity.java
@@ -2,9 +2,14 @@ package br.com.imcProject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class IMCActivity extends Activity {
     /** Called when the activity is first created. */
+	EditText etPeso, etAltura, etResultado;
+	float pesoFloat, alturaFloat, resultadoFloat;
+	Button btCalcular;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
