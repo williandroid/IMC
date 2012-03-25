8393936ea79f211cca4799fd260f1cb2a9a17c2a
Willian <willian-ottoni@hotmail.com>
2012-03-25 10:41:06 -0300

    Modificado Arquivo main.xml incluido os campos de entrada e saida e o botão

--- gen/br/com/imcProject/R.java
+++ gen/br/com/imcProject/R.java
@@ -10,11 +10,21 @@ package br.com.imcProject;
 public final class R {
     public static final class attr {
     }
+    public static final class calculo {
+        public static final int btSoma=0x7f060000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class entrada {
+        public static final int altura=0x7f050001;
+        public static final int peso=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class saida {
+        public static final int resultado=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
