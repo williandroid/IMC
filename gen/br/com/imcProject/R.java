698a09b80f3177bf6a58f7a6fac1972219d9a7e1
Willian <willian-ottoni@hotmail.com>
2012-05-23 12:55:14 -0300

    Data funcionando mas com problemas no ficheiros

--- gen/br/com/imcProject/R.java
+++ gen/br/com/imcProject/R.java
@@ -23,17 +23,12 @@ public final class R {
         public static final int peso=0x7f060000;
     }
     public static final class id {
-        public static final int historico1=0x7f050001;
-        public static final int historico2=0x7f050003;
-        public static final int historico3=0x7f050005;
-        public static final int listView=0x7f050006;
-        public static final int textView1=0x7f050000;
-        public static final int textView2=0x7f050002;
-        public static final int textView3=0x7f050004;
-        public static final int tvAltura=0x7f050008;
-        public static final int tvCondicao=0x7f05000a;
-        public static final int tvPeso=0x7f050007;
-        public static final int tvResultado=0x7f050009;
+        public static final int historico1=0x7f050000;
+        public static final int listView=0x7f050001;
+        public static final int tvAltura=0x7f050003;
+        public static final int tvCondicao=0x7f050005;
+        public static final int tvPeso=0x7f050002;
+        public static final int tvResultado=0x7f050004;
     }
     public static final class layout {
         public static final int ficheiros=0x7f030000;
